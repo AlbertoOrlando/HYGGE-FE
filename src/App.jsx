8635d19ff4e81f5df637a4257a8b28172f7c67ca
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-03-21 16:31:51 +0100

    feat: restructure routing by removing ContactUsPage and updating AboutUsPage layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,23 @@ import DefaultLayout from "./layout/DefaultLayout";
 // Pages
 import AboutUsPage from "./pages/AboutUsPage";
 import HomePage from "./pages/HomePage";
-import ContactUsPage from "./pages/ContactUsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
 
   return (
     <>
-       <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />} >
-            <Route path="/" element={<HomePage />} /> 
+            <Route path="/" element={<HomePage />} />
             <Route path="/about-us" element={<AboutUsPage />} />
             <Route path="/contact-us" element={<ContactUsPage />} />
             <Route path="/*" element={<NotFoundPage />} />
           </Route>
         </Routes>
       </BrowserRouter >
- 
+
     </>
   )
 }
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,7 +1,16 @@
 export default function AboutUsPage() {
     return (
         <>
-          
+            <div className="container_about">
+                <div>
+                    <h1>About</h1>
+                    <P></P>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
         </>
     );
 }
