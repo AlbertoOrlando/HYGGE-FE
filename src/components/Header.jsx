020f893e4c54a810ecd76396937dbda77421a3a0
Elisei <eliseivlas.002@gmail.com>
2025-03-21 12:01:13 +0100

    MODIFICA HEADER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
+import {NavLink, Link} from "react-router-dom"
+
+import "../components-CSS/";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faPlus } from "@fortawesome/free-solid-svg-icons";
+
 export default function Header() {
     return (
         <>
-          
+            <div className="codice-sconto"><p>20% SCONTO Per acquisti superiori a 299,99€ Codice: HYGGE-20</p></div>
+            <div className="header-container">
+                <div>
+                    <Link>LOGO</Link>
+                </div>
+                <div>
+                    <NavLink to="/">Camera da letto</NavLink>
+                    <NavLink to="/">Bagno</NavLink>
+                    <NavLink to="/">Saloto</NavLink>
+                    <NavLink to="/">Sala da pranzo</NavLink>
+                    <NavLink to="/">Giardino</NavLink>
+                    <NavLink to="/">Garage</NavLink>
+                    <NavLink to="/"></NavLink>
+
+                </div>
+                <div>
+                    <div>
+                        <Link><FontAwesomeIcon icon={faHome} /></Link>
+                        <Link><FontAwesomeIcon icon={faPlus} /></Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
