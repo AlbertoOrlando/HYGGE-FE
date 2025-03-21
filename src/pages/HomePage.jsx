7251e0f33338119b932465cec3c4b4bd50e75419
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-21 13:29:50 +0100

    [feat] add homepage img, navbar sticky

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import "../components-CSS/HomepageCSS.css"
+
 export default function HomePage() {
     return (
         <>
-          
+            <div className="content-home">
+            </div>
         </>
     );
 }
