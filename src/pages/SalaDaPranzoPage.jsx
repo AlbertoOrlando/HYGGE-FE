6de182290f46d0c49bf750ac51aeb6349ada2b07
Elisei <eliseivlas.002@gmail.com>
2025-03-21 19:58:45 +0100

    aggiunta pagine categorie

---
+++ src/pages/SalaDaPranzoPage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SalaDaPranzoPage() {
+    return (
+        <>
+           SalaDaPranzoPage
+        </>
+    );
+}
---
+++ src/pages/SalottoPage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function SalottoPage() {
+    return (
+        <>
+           SalottoPage
+        </>
+    );
+}
